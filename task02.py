5537a4f8fb2475937053d2ce3364f496c8741e8d
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-19 10:42:05 +0500

    bajarildi

---
+++ task02.py
@@ -0,0 +1,12 @@
+son01 = int(input("Birinchi sonni kiriting:"))
+son02 = int(input("Ikkinchi sonni kiriting:"))
+
+if son01 > son02:
+    print(son01)
+
+if son01 < son02:
+    print(son02)
+
+if son01 == son02:
+    print("teng")
+    
