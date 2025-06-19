c8f8163c2e722e5f73f57f8f9e4714dc3bb19efc
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-19 11:22:24 +0500

    bajarildi

---
+++ task10.py
@@ -0,0 +1,17 @@
+bal = int(input("Imtihon balini kiriting( 0 dan 100 gacha):"))
+
+if bal >= 90 and bal <= 100:
+    print("A")
+
+if bal >= 80 and bal <= 89:
+    print("B")
+
+if bal >= 70 and bal <= 79:
+    print("C")
+
+if bal >= 60 and bal <= 69:
+    print("D")
+
+if bal > 0 and bal <= 59:
+    print("F")
+                 
