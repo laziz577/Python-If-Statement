855c10b7922abcb29a06ee8fb42cb8e4fc8be5ca
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-19 11:16:30 +0500

    bajarildi

---
+++ task09.py
@@ -0,0 +1,13 @@
+a = float(input("Uchburchak tomonini kiriting 1:"))
+b= float(input("Uchburchak tomonini kiriting 2:"))
+c = float(input("Uchburchak tomonini kiriting 3:"))
+
+if a == b == c:
+    print("Uchburchak teng tomonli")
+
+if a == b < c:
+    print("Uchburchak teng yonli")
+
+if a < b < c:
+    print("Uchburchak turli tomonli")
+            
