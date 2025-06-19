f73caeb4ecf98831ef2a1c131fd4829395625861
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-19 11:30:25 +0500

    bajarildi

---
+++ task12.py
@@ -0,0 +1,12 @@
+pasword1 = input("parolni kiriting:")
+pasword2 = input("parolni kiriting:")
+
+if pasword1 == pasword2:
+    print("Parol qabul qilindi")
+
+
+else:
+    print("Parollar mos emas") 
+       
+
+    
