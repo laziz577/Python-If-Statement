26246706089470f02547c77472d93adf58de31e1
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-19 11:00:32 +0500

    bajarildi

---
+++ task06.py
@@ -0,0 +1,14 @@
+number = str(input("Telefon raqamini kiriting:"))
+kod = number[:2]
+if kod == "90" or kod == "91":
+    print("Beeline")
+
+if kod == "93" or kod == "94":
+    print("Ucell")
+
+if kod =="95" or kod == "97":
+    print("Uzmobile")
+
+if kod == "88" or kod == "99":
+    print("Mobiuz")
+       
